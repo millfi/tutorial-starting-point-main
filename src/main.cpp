645ad82a761439db280276bf2f3a8e4ca0fa6009
millfi <millfi@outlook.jp>
2025-05-27 10:04:42 +0900

    task graph

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include "window.hpp"
+#include "shaders/shared.inl"
+
+#include <daxa/utils/pipeline_manager.hpp>
+#include <daxa/utils/task_graph.hpp>
+void upload_vertex_data_task(daxa::TaskGraph& tg, daxa::TaskBufferView vertices)
+{
+	tg.add_task({
+		.attachments = {
+			daxa::inl_attachment(daxa::TaskBufferAccess::TRANSFER_WRITE, vertices),
+		},
+		.task = [=](daxa::TaskInterface ti)
+		{
+
+		}
+	});
+	
+}
+int main(int argc, char const *argv[]){
+	auto window = AppWindow("Learn Daxa", 860, 600);
+
+	daxa::Instance instance = daxa::create_instance({});
+	daxa::Device device = instance.create_device_2(instance.choose_device({}, {}));
+	daxa::Swapchain swapchain = device.create_swapchain({
+		.native_window = window.get_native_handle(),
+		.native_window_platform = window.get_native_platform(),
+		.present_mode = daxa::PresentMode::FIFO,
+		.image_usage = daxa::ImageUsageFlagBits::TRANSFER_DST,
+		.name = "my swapchain"
+	});
+	while(!window.should_close()) {
+		window.update();
+	}
+
+	    auto pipeline_manager = daxa::PipelineManager({
+        .device = device,
+        .shader_compile_options = {
+            .root_paths = {
+                DAXA_SHADER_INCLUDE_DIR,
+                "./src/shader",
+            },
+            .language = daxa::ShaderLanguage::GLSL,
+            .enable_debug_info = true,
+        },
+        .name = "my pipeline manager"
+    });
+
+	std::shared_ptr<daxa::RasterPipeline> pipeline;
+	auto result = pipeline_manager.add_raster_pipeline({
+		.vertex_shader_info = daxa::ShaderCompileInfo{.source = daxa::ShaderFile{"main.glsl"}},
+		.fragment_shader_info = daxa::ShaderCompileInfo{.source = daxa::ShaderFile{"main.glsl"}},
+		.color_attachments = {{.format = swapchain.get_format()}},
+		.raster = {},
+		.push_constant_size = sizeof(MyPushConstant),
+		.name = "my pipeline"
+	});
+	if(result.is_err()){
+		std::cerr << result.message() << std::endl;
+		return -1;
+	}
+	pipeline = result.value();
+	auto buffer_id = device.create_buffer({
+		.size = sizeof(MyVertex) * 3,
+		.name = "my vertex data"
+	});
+	return 0;
+}
---
+++ src/window.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <daxa/daxa.hpp>
+using namespace daxa::types; // For types like `u32`
+
+#include <GLFW/glfw3.h>
+
+#if defined(_WIN32)
+	#define GLFW_EXPOSE_NATIVE_WIN32
+	#define GLFW_NATIVE_INCLUDE_NONE
+	using HWND = void*;
+#elif defined(__linux__)
+	#define GLFW_EXPOSE_NATIVE_X11
+	#define GLFW_EXPOSE_NATIVE_WAYLAND
+#endif
+
+#include <GLFW/glfw3native.h> // Platform-specific GLFW functions
+
+struct AppWindow {
+	GLFWwindow* glfw_window_ptr;
+	u32 width, height;
+	bool minimized = false;
+	bool swapchain_out_of_date = false;
+
+	explicit AppWindow(char const* window_name, u32 sx = 800, u32 sy = 600)
+	: width{sx}, height{sy} {
+		glfwInit(); // Initialize GLFW
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // No graphics API
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);    // Make the window resizable
+
+		// Create the GLFW window
+		glfw_window_ptr = glfwCreateWindow(
+			static_cast<i32>(width), static_cast<i32>(height),
+			window_name, nullptr, nullptr
+		);
+
+		// Associate the AppWindow object with the GLFW window
+		glfwSetWindowUserPointer(glfw_window_ptr, this);
+
+		// Set a callback to handle resizing events
+		glfwSetWindowSizeCallback(
+			glfw_window_ptr, [] (GLFWwindow* window, int size_x, int size_y) 
+			{
+				auto* win = static_cast<AppWindow*>(glfwGetWindowUserPointer(window));
+				win->width = static_cast<u32>(size_x);
+				win->height = static_cast<u32>(size_y);
+				win->swapchain_out_of_date = true;
+			}
+		);
+	}
+	auto get_native_handle() const -> daxa::NativeWindowHandle {
+		#if defined(_WIN32)
+		return glfwGetWin32Window(glfw_window_ptr);
+		#elif defined(__linux__)
+		switch (get_native_platform()) {
+		case daxa::NativeWindowPlatform::WAYLAND_API:
+			return reinterpret_cast<daxa::NativeWindowHandle>(glfwGetWaylandWindow(glfw_window_ptr));
+		case daxa::NativeWindowPlatform::XLIB_API:
+		default:
+			return reinterpret_cast<daxa::NativeWindowHandle>(glfwGetX11Window(glfw_window_ptr));
+		
+		#endif
+	}
+	static auto get_native_platform() -> daxa::NativeWindowPlatform {
+		switch (glfwGetPlatform()) {
+			case GLFW_PLATFORM_WIN32: return daxa::NativeWindowPlatform::WIN32_API;
+			case GLFW_PLATFORM_X11: return daxa::NativeWindowPlatform::XLIB_API;
+			case GLFW_PLATFORM_WAYLAND: return daxa::NativeWindowPlatform::WAYLAND_API;
+			default: return daxa::NativeWindowPlatform::UNKNOWN;
+		}
+	}
+	inline void set_mouse_capture(bool should_capture) const {
+		glfwSetCursorPos(glfw_window_ptr, static_cast<f64>(width / 2.), static_cast<f64>(height / 2.));
+		glfwSetInputMode(glfw_window_ptr, GLFW_CURSOR, should_capture ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL);
+		glfwSetInputMode(glfw_window_ptr, GLFW_RAW_MOUSE_MOTION, should_capture);
+	}
+	inline bool should_close() const {
+		return glfwWindowShouldClose(glfw_window_ptr);
+	}
+	inline void update() const {
+		glfwPollEvents();
+		glfwSwapBuffers(glfw_window_ptr);
+	}
+	inline GLFWwindow* get_glfw_window() const {
+		return glfw_window_ptr;
+	}
+ };
